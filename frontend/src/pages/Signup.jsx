1fe69e4f25835a916a2afbacfffb617b00bddf8a
Sushanth1425 <sushanthb1425@gmail.com>
2026-02-04 20:42:49 +0530

    added login page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import API from '../services/api';
 
 const Signup = () => {
+  const [name, setName]= useState('');
+  const [email, setEmail]= useState('');
+  const [pwd, setPwd]= useState('')
+  const navigate= useNavigate()
+
+  const handleSignup= async(e)=>{
+    e.preventDefault();
+    await API.post('/auth/signup', {name, email, password: pwd})
+    navigate('/login')
+  }
   return (
-    <div>Signup</div>
+    <div className="container mt-5">
+      <h3>Signup</h3>
+      <form onSubmit={handleSignup}>
+        <input className="form-control mb-2" placeholder="Name" onChange={e=>setName(e.target.value)} />
+        <input className="form-control mb-2" placeholder="Email" onChange={e=>setEmail(e.target.value)} />
+        <input className="form-control mb-2" type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)} />
+        <button className="btn btn-primary">Signup</button>
+      </form>
+    </div>
   )
 }
 
