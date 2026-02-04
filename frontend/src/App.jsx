65cced1cfbf089d526bdc6dc787815299de2b0e5
Sushanth1425 <sushanthb1425@gmail.com>
2026-02-04 19:51:29 +0530

    changed app.jax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import PrivateRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import MyCourses from './pages/MyCourses'
+import CourseDetail from './pages/CourseDetail'
 
 function App() {
 
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<PrivateRoute> <Home /> </PrivateRoute>}  />
+        <Route path='/courses/:id' element={<PrivateRoute> <CourseDetail /> </PrivateRoute>}  />
+        <Route path='/my-courses' element={<PrivateRoute> <MyCourses /> </PrivateRoute>}  />
+      </Routes>
+    </>
   )
 }
 
