9a38ff6f71987f39b57cf44d79775a1f42de1e73
Sushanth1425 <sushanthb1425@gmail.com>
2026-02-04 19:03:06 +0530

    fixed bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import BrowserRouter from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
