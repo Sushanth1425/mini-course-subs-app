a3c3cb38910ab3fb79eb2825018773306c006d83
Sushanth1425 <sushanthb1425@gmail.com>
2026-02-04 20:44:35 +0530

    added home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import API from '../services/api'
+import CourseCard from '../components/CourseCard';
 
 const Home = () => {
+  const [courses, setCourses]= useState([])
+
+  useEffect(()=> API.get('/courses').then(res=>setCourses(res.data)), [])
+
   return (
-    <div>Home</div>
+    <div className="container mt-4">
+      <h3>Courses</h3>
+      <div className="row">
+        {courses.map(course => <CourseCard key={course._id} course={course} />)}
+      </div>
+    </div>
   )
 }
 
