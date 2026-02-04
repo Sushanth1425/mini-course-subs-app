2e57b2db8b0d1ea00012151ceca5f3818d82d3a4
Sushanth1425 <sushanthb1425@gmail.com>
2026-02-04 19:52:59 +0530

    added login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import API from '../services/api';
 
 const Login = () => {
+  const [email, setEmail]= useState('');
+  const [pwd, setPwd]= useState('');
+  const navigate= useNavigate();
+
+  const handleLogin= async(e)=>{
+    e.preventDefault();
+    try {
+      const res= await API.post('/auth/login', {email, password: pwd});
+      localStorage.setItem("token", res.data.token);
+      navigate('/')
+    } catch (err) {
+      alert("Invalid Credentials")
+    }
+  }
   return (
-    <div>Login</div>
+    <div className="container mt-5">
+      <h3>Login</h3>
+      <form action="" onSubmit={handleLogin}>
+        <input type="text" className='form-control mb-2' placeholder='Email' onChange={e=>setEmail(e.target.value)} />
+        <input type="password" className='form-control mb-2' placeholder='Password' onChange={e=>setPwd(e.target.value)} />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
